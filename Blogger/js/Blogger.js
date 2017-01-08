df420573e44031160f367fa8d393bba31a878674
Tahir Hassan <t.a.hassan@gmail.com>
2017-01-08 16:57:54 +0000

    moved the blogUtils class outside of the jquery anonymous function

--- Blogger/js/Blogger.js
+++ Blogger/js/Blogger.js
@@ -1,55 +1,55 @@
+var blogUtils = {};
+
+blogUtils.replaceBacktickWithVar = function (body) {
+    var closed = true;
+    var output = "";
+    var index = 0;
+    while (index < body.length) {
+        if (body[index] === '`' && body[index + 1] === '`') {
+            output = output + "`";
+            index += 2;
+        }
+        else if (body[index] === '`') {
+            var varTag = closed ? '<var>' : '</var>';
+            output = output + varTag;
+            closed = !closed;
+            index++;
+        }
+        else {
+            output = output + body[index];
+            index++;
+        }
+    }
+
+    if (!closed) {
+        alert('body not closed. Click ok to see body text.');
+        alert(body);
+    }
+
+    return output;
+};
+
+blogUtils.substituteBacktickWithVar_ignoreTags = ['PRE', 'CODE', 'VAR', 'SCRIPT'];
+
+blogUtils.substituteBacktickWithVar = function ($elem) {
+
+    $elem.contents().each(function (i, e) {
+        if (e.nodeType === 1) {
+            // e is an element
+            if (blogUtils.substituteBacktickWithVar_ignoreTags.indexOf(e.nodeName) === -1) {
+                // e is not a pre, code, var or script element, therefore recurse over it
+                blogUtils.substituteBacktickWithVar($(e));
+            }
+        } else if (e.nodeType === 3) {
+            // text node here
+            if (e.nodeValue.includes('`')) {
+                $(e).replaceWith(blogUtils.replaceBacktickWithVar(e.nodeValue));
+            }
+        }
+    });
+};
+
 $(function () {
-    var blogUtils = {};
-
-    blogUtils.replaceBacktickWithVar = function (body) {
-        var closed = true;
-        var output = "";
-        var index = 0;
-        while (index < body.length) {
-            if (body[index] === '`' && body[index + 1] === '`') {
-                output = output + "`";
-                index += 2;
-            }
-            else if (body[index] === '`') {
-                var varTag = closed ? '<var>' : '</var>';
-                output = output + varTag;
-                closed = !closed;
-                index++;
-            }
-            else {
-                output = output + body[index];
-                index++;
-            }
-        }
-
-        if (!closed) {
-            alert('body not closed. Click ok to see body text.');
-            alert(body);
-        }
-
-        return output;
-    };
-
-    blogUtils.substituteBacktickWithVar_ignoreTags = ['PRE', 'CODE', 'VAR', 'SCRIPT'];
-
-    blogUtils.substituteBacktickWithVar = function ($elem) {
-
-        $elem.contents().each(function (i, e) {
-            if (e.nodeType === 1) {
-                // e is an element
-                if (blogUtils.substituteBacktickWithVar_ignoreTags.indexOf(e.nodeName) === -1) {
-                    // e is not a pre, code, var or script element, therefore recurse over it
-                    blogUtils.substituteBacktickWithVar($(e));
-                }
-            } else if (e.nodeType === 3) {
-                // text node here
-                if (e.nodeValue.includes('`')) {
-                    $(e).replaceWith(blogUtils.replaceBacktickWithVar(e.nodeValue));
-                }
-            }
-        });
-    };
-    
     blogUtils.substituteBacktickWithVar($('div.post-body'));
     $('body').removeClass('wait-backtickvar');
 });
