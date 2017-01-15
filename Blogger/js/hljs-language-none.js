eba06cee61277d7ba695c7a441653f5f210826a1
Tahir Hassan <t.a.hassan@gmail.com>
2017-01-15 20:22:38 +0000

    returning an empty object for the none language - to see if this will work

--- Blogger/js/hljs-language-none.js
+++ Blogger/js/hljs-language-none.js
@@ -1,39 +1,5 @@
 hljs.registerLanguage("none", function(e) {
-    var r= {
-        b: /`[\s\S]/
-    }
-    ;
     return {
-        cI:!0, k: {
-            keyword: "", literal: "", built_in: ""
-        }
-        , c:[ {
-            cN: "", b: ""
-        }
-        , r, e.inherit(e.QSM, {
-            c: [r]
-        }
-        ), e.C(";", "$", {
-            r: 0
-        }
-        ), {
-            cN: "number", b: e.NR, r: 0
-        }
-        , {
-            cN: "variable", b: "%", e: "%", i: "\\n", c: [r]
-        }
-        , {
-            cN:"symbol", c:[r], v:[ {
-                b: '^[^\\n";]+::(?!=)'
-            }
-            , {
-                b: '^[^\\n";]+:(?!=)', r: 0
-            }
-            ]
-        }
-        , {
-            b: ",\\s*,"
-        }
-        ]
+        
     }
 });
